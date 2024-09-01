61e9172c16c1afc456342565aa4e6c2043a2ed44
SAHILGAWLI <sahilgawliinft-@atharvacoe.ac.in>
2024-09-01 20:06:35 +0530

    'update'

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,19 +1,11 @@
-// src/components/Loader.js
 import React from 'react';
-import { ThreeDots } from 'react-loader-spinner';
+import './Loader.css'; // Import CSS for styling
 
-const Loader = () => {
-    return (
-        <div className="loader-container">
-            <ThreeDots
-                height="100"
-                width="100"
-                radius="9"
-                color="#4fa94d"
-                ariaLabel="three-dots-loading"
-            />
-        </div>
-    );
-};
+const Loader = () => (
+  <div className="loader">
+    <div className="spinner"></div>
+    <p>Processing your file, please wait...</p>
+  </div>
+);
 
 export default Loader;
